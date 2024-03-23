0af7a36e31ed5856754be227449f41b8dc16300b
Lencia Rodrigues <rodrigueslenica2004@gmail.com>
2024-03-23 20:44:44 +0530

    factorial

---
+++ ISA2_207/src/Q2/factorial.java
@@ -0,0 +1,16 @@
+package Q2;
+
+public class factorial {
+	
+	public static void main(String[]args) {
+		//TODO Auto- 
+	int num=10;
+	long factorial =1;
+	for (int i=1;i<=num;i++) 
+	{
+		factorial=factorial*i;
+		}
+	System.out.printf("Factroail of a %d=%d",num,factorial);
+	}
+}
+
