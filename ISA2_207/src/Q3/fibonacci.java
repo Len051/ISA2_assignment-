75c24230fa40224eb600aadce1a37a8dc789ca09
Lencia Rodrigues <rodrigueslenica2004@gmail.com>
2024-03-23 21:03:52 +0530

    fibonacci

---
+++ ISA2_207/src/Q3/fibonacci.java
@@ -0,0 +1,27 @@
+package Q3;
+
+import java.util.Scanner;
+
+public class fibonacci {
+	    public static void main(String[] args) {
+	        Scanner scanner = new Scanner(System.in);
+	        System.out.print("Enter the number of terms in Fibonacci series: ");
+	        int n = scanner.nextInt();
+	        scanner.close();
+
+	        System.out.println("Fibonacci Series:");
+	        displayFibonacci(n);
+	    }
+
+	    public static void displayFibonacci(int n) {
+	        int num1 = 0, num2 = 1;
+	        for (int i = 1; i <= n; ++i) {
+	            System.out.print(num1 + " ");
+	            int sum = num1 + num2;
+	            num1 = num2;
+	            num2 = sum;
+	        }
+	    }
+	}
+
+
