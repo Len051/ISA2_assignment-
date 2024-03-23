993e87b451d9fd7ff560e7ccc46ae5e3ae3477f1
Lencia Rodrigues <rodrigueslencia2004@gmail.com>
2024-03-23 14:33:31 +0530

    Q1

---
+++ ISA2_207/src/Q1/addintegers.java
@@ -0,0 +1,22 @@
+package Q1;
+
+public class addintegers {
+
+
+	public static  int add(int a,int b) {
+		return a+b;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int num1=10;
+		int num2=20;
+		
+		int sum=add(num1,num2);
+		System.out.println("The sum of two numbers = "+sum);
+
+	
+
+}
+
+}
