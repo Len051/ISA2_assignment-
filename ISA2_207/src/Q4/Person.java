b32fa62254812e61fa49ac6fd8aa32257889079c
Lencia Rodrigues <rodrigueslenica2004@gmail.com>
2024-03-23 22:16:42 +0530

    person

--- ISA2_207/src/Q4/Person.java
+++ ISA2_207/src/Q4/Person.java
@@ -1,26 +1,27 @@
 package Q4;
 
 public class Person {
+
+		private String name;
+		private int age;
+
+			//Constructor
+			Person(String name, int age)
+			{
+			this.name=name;
+			this.age=age;
+			}
+			public String getname() {
+				return name;
+			}
+
+			public int getage() { 
+				return age;
+			}
+
 	
-	private String name;
-	private int age;
-
-		//Constructor
-		Person(String name, int age)
-		{
-		this.name=name;
-		this.age=age;
-		}
-		public String getname() {
-			return name;
-		}
-
-		public int getage() { 
-			return age;
-		}
-
-		public static void main(String[] args) {
-		//TODO Auto-generated method stub
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
 
 		Person obl=new Person("Kevin", 22);
 		Person ob2=new Person("Aaron",19);
@@ -28,5 +29,6 @@ public class Person {
 		System.out.println(obl.getname()+" is "+obl.getage()+" years");
 		System.out.println(ob2.getname()+" is "+ob2.getage ()+" years");
 
-}
+	}
+
 }
