1dbdb3040d64f8e5e2f02cd93770dc72391082b7
Lencia Rodrigues <rodrigueslenica2004@gmail.com>
2024-03-24 10:17:57 +0530

    person and person_junit

--- ISA2_207/src/Q4/Person.java
+++ ISA2_207/src/Q4/Person.java
@@ -11,23 +11,28 @@ public class Person {
 			this.name=name;
 			this.age=age;
 			}
-			public String getname() {
+			public String getName() {
 				return name;
 			}
-
-			public int getage() { 
-				return age;
+			public void setName(String name) {
+				this.name=name;
 			}
 
-	
+			public int getAge() { 
+				return age;
+			}
+			public void getAge(int age) {
+				this.age=age;
+			}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		Person obl=new Person("Kevin", 22);
-		Person ob2=new Person("Aaron",19);
+		//Person obl=new Person("Kevin", 22);
+		//Person ob2=new Person("Aaron",19);
 
-		System.out.println(obl.getname()+" is "+obl.getage()+" years");
-		System.out.println(ob2.getname()+" is "+ob2.getage ()+" years");
+	//	System.out.println(obl.getName()+" is "+obl.getAge()+" years");
+		//System.out.println(ob2.getName()+" is "+ob2.getAge ()+" years");
 
 	}
 
---
+++ ISA2_207/src/Q4/PersonTest.java
@@ -0,0 +1,38 @@
+package Q4;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PersonTest {
+
+	@Test
+	public void test() {
+		
+		Person person1=new Person("Kevin",22);
+		assertEquals("Kevin",person1.getName());
+		assertEquals(22,person1.getAge());
+		
+		Person person2=new Person("Aaron",19);
+		assertEquals("Aaron",person2.getName());
+		assertEquals(19,person2.getAge());
+		
+	}
+	@Test
+	 public void testConstructor() {
+	        String ob1 = "Kevin";
+	        int ob2 = 22;
+	        Person person1 = new Person(ob1, ob2);
+	        assertEquals(ob1, person1.getName());
+	        assertEquals(ob2, person1.getAge());
+	        
+	        String ob3 = "Kevin";
+	        int ob4 = 22;
+	        Person person2 = new Person(ob3, ob4);
+	        assertEquals(ob3, person2.getName());
+	        assertEquals(ob4, person2.getAge());
+	    }
+	
+	
+
+}
